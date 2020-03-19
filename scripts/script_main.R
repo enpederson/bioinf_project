1b59d8c8f40ac5e570ecc233bdce9fef71d3dce2
Eric Pederson <epederso@scc-yq1.scc.bu.edu>
2020-03-19 14:41:29 -0400

    merge shortreads before BLAST

--- scripts/script_main.R
+++ scripts/script_main.R
@@ -29,6 +29,8 @@ library(forcats)
 Sys.setenv(PATH=paste(Sys.getenv("PATH"), "/share/pkg.7/blast+/2.7.1/install/bin", sep=":"))
 Sys.setenv(PATH=paste(Sys.getenv("PATH"), "/share/pkg.7/sratoolkit/2.9.2/install/bin/", sep=":"))
 
+#access local BLAST database
+bl <- blast(db="/projectnb/ct-shbioinf/blast/nt.fa") 
 
 #read Swamp SRRs:
 srrS1=c('SRX7820012')
@@ -43,9 +45,15 @@ dnaS1 = readFastq('.', pattern=srrS1)
 dnaS2 = readFastq('.', pattern=srrS2)
 dnaS3 = readFastq('.', pattern=srrS3)
 
-readS1 = sread(dnaS1, id=id(dnaS1))
-readS2 = sread(dnaS2, id=id(dnaS2))
-readS3 = sread(dnaS3, id=id(dnaS3))
+#combine Swamp reads
+S123 = c(dnaS1,dnaS2,dnaS3)
+dnaMergeS <- do.call('append', S123)
+
+#readS1 = sread(dnaS1, id=id(dnaS1))
+#readS2 = sread(dnaS2, id=id(dnaS2))
+#readS3 = sread(dnaS3, id=id(dnaS3))
+readMergeS = sread(dnaMergeS, id=id(dnaMergeS))
+
 
 #read O'Hara SRRs:
 srrOH1=c('SRX7820009')
@@ -60,11 +68,18 @@ dnaOH1 = readFastq('.', pattern=srrOH1)
 dnaOH2 = readFastq('.', pattern=srrOH2)
 dnaOH3 = readFastq('.', pattern=srrOH3)
 
-readOH1 = sread(dnaOH1, id=id(dnaOH1))
-readOH2 = sread(dnaOH2, id=id(dnaOH2))
-readOH3 = sread(dnaOH3, id=id(dnaOH3))
+OH123 = c(dnaOH1,dnaOH2,dnaOH3)
+dnaMergeOH <- do.call('append', OH123)
 
-#access local BLAST database
-bl <- blast(db="/projectnb/ct-shbioinf/blast/nt.fa") 
+#readOH1 = sread(dnaOH1, id=id(dnaOH1))
+#readOH2 = sread(dnaOH2, id=id(dnaOH2))
+#readOH3 = sread(dnaOH3, id=id(dnaOH3))
+readMergeOH = sread(dnaMergeOH, id=id(dnaMergeOH))
+
+
+
+#This will take a long time. Run once and save environment!
+clS <- predict(bl, readMergeS, BLAST_args = '-num_threads 12 -evalue 1e-100') #swamp
+clOH <- predict(bl, readMergeOH, BLAST_args = '-num_threads 12 -evalue 1e-100') #O'Hara
 
 
